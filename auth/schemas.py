5d381ef3db321b80aa5c0b42c9d00f832c3973a9
Ivan <ponomarev777@list.ru>
2024-07-26 18:34:52 +0300

    Lesson 5 done!

---
+++ auth/schemas.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from fastapi_users import schemas
+from fastapi_users.schemas import PYDANTIC_V2
+from pydantic import EmailStr
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: EmailStr
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    email: EmailStr
+    password: str
+    role_id: int
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
--- models/models.py
+++ models/models.py
@@ -1,24 +1,32 @@
-from datetime import datetime
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+import datetime
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import TIMESTAMP, String, Boolean, ForeignKey
 
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        datetime.datetime: TIMESTAMP(timezone=True)
+    }
 
 
-class Roles(Base):
-    __tablename__ = "roles"
+class Role(Base):
+    __tablename__ = "role"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     permissions: Mapped[str] = mapped_column(nullable=False)
 
 
-class Users(Base):
-    __tablename__ = "users"
+class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    registered_at: Mapped[datetime] = datetime.now
-    role_id: Mapped[int] = relationship("Roles", back_populates="roles.id")
+    registered_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
+    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"))
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
